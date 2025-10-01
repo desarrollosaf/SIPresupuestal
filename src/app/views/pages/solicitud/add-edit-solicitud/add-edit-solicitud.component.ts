6d75701dd9cb5132a1eadda9eebdda80e78cca2f
cescDG <cesar199403@hotmail.com>
2025-10-01 16:09:09 -0600

    pago

--- src/app/views/pages/solicitud/add-edit-solicitud/add-edit-solicitud.component.ts
+++ src/app/views/pages/solicitud/add-edit-solicitud/add-edit-solicitud.component.ts
@@ -8,10 +8,12 @@ import {
   inject,
   signal,
   computed,
-  effect
+  effect,
+  ViewChild,
+  TemplateRef
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-add-edit-solicitud',
 
@@ -38,20 +40,22 @@ export class AddEditSolicitudComponent {
     { id: '2', nombre: 'GASTOS A COMPROBAR' },
     { id: '3', nombre: 'PARA PAGO' },
   ];
+
   opcionesUnidad = ['UNIDAD 1', 'UNIDAD 2', 'UNIDAD 3'];
   opcionesActividad = ['ACTIVIDAD A', 'ACTIVIDAD B', 'ACTIVIDAD C'];
   opcionesPartida = ['PARTIDA X', 'PARTIDA Y', 'PARTIDA Z'];
   opcionesPais = ['México', 'Estados Unidos', 'Europa'];
   opcionesFacturaExtranjera = ['Sí', 'No'];
-  opcionesCuentaConFactura = ['Sí', 'No'];
+
+  opcionesCuentaConFactura = [
+    { id: '1', nombre: 'Sí' },
+    { id: '2', nombre: 'No' }
+  ];
   documentos: { [campo: string]: File[] } = {};
   documentossoli1: { [campo: string]: File[] } = {};
   documentossoli2: { [campo: string]: File[] } = {};
-
-
-
-
-
+  modalRef: NgbModalRef;
+  rfcInvalido: boolean = false;
 
   detalles: any[] = [];
   validacionCompr = false;
@@ -61,13 +65,13 @@ export class AddEditSolicitudComponent {
     partida: '',
     importe: null,
     cuenta_con_factura: '',
-    rfc_emisor: 'N/A',
-    nombre_emisor: 'N/A',
-    concepto: 'N/A',
-    folio: 'N/A',
-    pais: 'N/A',
-    factura_extranjera: 'N/A',
-    tax_id: 'N/A',
+    rfc_emisor: '',
+    nombre_emisor: '',
+    concepto: '',
+    folio: '',
+    pais: '',
+    factura_extranjera: '',
+    tax_id: '',
   };
 
   nuevoDetalle1: any = {
@@ -76,16 +80,17 @@ export class AddEditSolicitudComponent {
     partida: '',
     importe: null,
   };
+  @ViewChild('xlModal', { static: true }) xlModal!: TemplateRef<any>;
 
   get mostrarFormularioSimple() {
     const val = this.formSolicitud.get('id_programatico_presupuestal')?.value;
     return val === '1' || val === '2';
   }
-
   get mostrarFormularioFactura() {
     return this.formSolicitud.get('id_programatico_presupuestal')?.value === '3';
   }
-  constructor(private fb: FormBuilder) {
+
+  constructor(private fb: FormBuilder, private modalService: NgbModal) {
     this.formSolicitud = this.fb.group({
       folio: ['', Validators.required],
       fecha_solicitud: ['', Validators.required],
@@ -101,41 +106,17 @@ export class AddEditSolicitudComponent {
       importe: ['', Validators.required],
       importe_letra: ['', Validators.required],
       observaciones: ['', Validators.required],
-
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     this.formSolicitud.get('id_programatico_presupuestal')?.valueChanges.subscribe(valor => {
       if (valor === '1' || valor === '2') {
         this.detalles = [];
         this.resetNuevoDetalle();
       }
-
       if (valor === '3') {
         this.detalles = [];
         this.resetNuevoDetalle();
       }
     });
-
-
-
-
-
     this.formSolicitud.get('tipo_beneficiario')?.valueChanges.subscribe((tipo) => {
       if (tipo === 'servidor') {
         this.beneficiarios = this.servidoresPublicos;
@@ -147,27 +128,58 @@ export class AddEditSolicitudComponent {
       }
       this.formSolicitud.get('id_beneficiario')?.reset();
     });
+
+
+
   }
 
-  agregarDetalleSimple() {
-const { unidad, actividad, partida, importe,rfc_emisor,nombre_emisor,concepto,folio,pais,factura_extranjera,tax_id,cuenta_con_factura } = this.nuevoDetalle;
-console.log(cuenta_con_factura);
-    if (unidad && actividad && partida && importe > 0) {
-  
+
+  onCambioFacturaExtranjera(valor: string) {
+    if (valor !== '1') {
+      this.nuevoDetalle.tax_id = '';
+    }
+  }
+  abrirModal() {
+    console.log('ho');
+    this.modalRef = this.modalService.open(this.xlModal, { size: 'lg' });
+    this.modalRef.result.then((result) => {
+      console.log("Modal cerrado:", result);
+      this.resetFact();
+     
+    }).catch((res) => {
+      console.log("Modal cerrado por dismiss");
+      this.resetFact();
+    });
+  }
+
+  onCambioCuentaFactura(valorSeleccionado: any) {
+    console.log(valorSeleccionado.id);
+    if (valorSeleccionado.id == 2) {
+      console.log('sisdhj');
+      this.abrirModal();
+    }
+  }
+
+  agregarDetalle2() {
+    const { unidad, actividad, partida, importe, rfc_emisor, nombre_emisor, concepto, folio, pais, factura_extranjera, tax_id, cuenta_con_factura } = this.nuevoDetalle;
+    console.log(cuenta_con_factura);
+    if (unidad && actividad && partida && importe > 0 && cuenta_con_factura) {
       this.detalles.push({
         unidad,
         actividad,
         partida,
         importe: parseFloat(importe).toFixed(2),
-        rfc_emisor: cuenta_con_factura === 'Sí' ? 'N/A' : rfc_emisor,
-        nombre_emisor: cuenta_con_factura === 'Sí' ? 'N/A' : nombre_emisor,
-        concepto: cuenta_con_factura === 'Sí' ? 'N/A' : concepto,
-        folio: cuenta_con_factura === 'Sí' ? 'N/A' : folio,
-        pais: cuenta_con_factura === 'Sí' ? 'N/A' : pais,
-        factura_extranjera: cuenta_con_factura === 'Sí' ? 'N/A' : factura_extranjera,
-        tax_id: cuenta_con_factura === 'Sí' ? 'N/A' : tax_id,
+        rfc_emisor: cuenta_con_factura == '1' ? 'N/A' : rfc_emisor,
+        nombre_emisor: cuenta_con_factura == '1' ? 'N/A' : nombre_emisor,
+        concepto: cuenta_con_factura == '1' ? 'N/A' : concepto,
+        folio: cuenta_con_factura == '1' ? 'N/A' : folio,
+        pais: cuenta_con_factura == '1' ? 'N/A' : pais,
+        factura_extranjera: cuenta_con_factura == '1' ? 'N/A' : factura_extranjera,
+        tax_id: cuenta_con_factura == '1' ? 'N/A' : tax_id,
       });
-
+      if (this.modalRef) {
+            this.modalRef.close('');
+          }
       this.resetDetalle2();
     } else {
       Swal.fire({
@@ -219,24 +231,32 @@ console.log(cuenta_con_factura);
       importe: null,
     };
   }
-
-  resetDetalle2(){
-
-     this.nuevoDetalle = {
-       unidad: '',
-    actividad: '',
-    partida: '',
-    importe: null,
-    cuenta_con_factura: '',
-    rfc_emisor: 'N/A',
-    nombre_emisor: 'N/A',
-    concepto: 'N/A',
-    folio: 'N/A',
-    pais: 'N/A',
-    factura_extranjera: 'N/A',
-    tax_id: 'N/A',
+  resetFact(){
+      this.nuevoDetalle.cuenta_con_factura = '';
+      this.nuevoDetalle.rfc_emisor = '';
+      this.nuevoDetalle.nombre_emisor = '';
+      this.nuevoDetalle.concepto = '';
+      this.nuevoDetalle.folio = '';
+      this.nuevoDetalle.pais = '';
+      this.nuevoDetalle.factura_extranjera = '';
+      this.nuevoDetalle.tax_id = '';
+  }
+  resetDetalle2() {
+    this.nuevoDetalle = {
+      unidad: '',
+      actividad: '',
+      partida: '',
+      importe: null,
+      cuenta_con_factura: '',
+      rfc_emisor: '',
+      nombre_emisor: '',
+      concepto: '',
+      folio: '',
+      pais: '',
+      factura_extranjera: '',
+      tax_id: '',
     };
-   
+
   }
 
   actualizarImporteLetra() {
@@ -257,6 +277,8 @@ console.log(cuenta_con_factura);
       }
     }
   }
+
+
   numeroALetras(num: number): string {
     const unidades = [
       '', 'uno', 'dos', 'tres', 'cuatro', 'cinco',
@@ -402,6 +424,21 @@ console.log(cuenta_con_factura);
       }
     }
   }
+  validarRfc(valor: string) {
+    const regexRFC = /^([A-ZÑ&]{3,4})\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])[A-Z\d]{3}$/;
+    const rfcNormalizado = valor?.toUpperCase().trim();
+    const esValido = regexRFC.test(rfcNormalizado);
+    if (!esValido) {
+      this.nuevoDetalle.rfc_emisor = '';
+      Swal.fire({
+        icon: 'error',
+        title: 'RFC inválido',
+        text: 'El RFC ingresado no cumple con el formato requerido.',
+        confirmButtonColor: '#960948'
+      });
+    }
+  }
+
 
   enviarDatos() {
     console.log(this.formSolicitud.value);
